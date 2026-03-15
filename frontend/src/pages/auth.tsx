0a10c36079d9bc5fa9c0b572eee2cf3f1d050815
Phichayapa Phemphoon <phichayapa.phemphoon@gmail.com>
2026-03-15 19:13:08 +0700

    Update auth.tsx

--- frontend/src/pages/auth.tsx
+++ frontend/src/pages/auth.tsx
@@ -7,7 +7,7 @@ export default function AuthPage() {
   const [user, setUser] = useState<any>(null);
   const [loading, setLoading] = useState(true);
 
-  const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+  const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
   useEffect(() => {
     if (!router.isReady) return;
